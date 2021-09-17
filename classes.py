66eb5efa758f811d0e271a3c1963c84d1dc25b84
jackson-89 <ccc012105@coderacademy.edu.au>
2021-09-17 10:41:33 +1000

    got repr and action  methods working

--- classes.py
+++ classes.py
@@ -4,6 +4,9 @@ import random
 class Fruit():
     def __init__ (self):
         self.flavour, self.colour = random.choice(self.varieties)
+
+    def __repr__(self):
+        return f"<{self.flavour}, {self.colour}, {self.__class__.__name__}>"
     
 
 class Apple(Fruit):
@@ -17,6 +20,18 @@ class Pear(Fruit):
 class Tree():
     def __init__(self):
         self.fruits = []
+        
+    def __repr__(self):
+        return f"{self.fruit_type} tree"
+
+    def blossom(self):
+        for i in range(self.fecundity):
+            self.fruits.append(self.fruit_type())
+
+    def harvest(self):
+        crop = self.fruits
+        self.fruits = []
+        return crop
     
 class AppleTree(Tree):
     fecundity = 8
@@ -38,4 +53,7 @@ class Cider():
         }
         for fruit in fruitlist:
             self.flavour[fruit.flavour] += 1
+
+    def __repr__(self):
+        return f"a barrel of {max(self.flavour,  key=lambda key:self.flavour[key])} cider"
 
